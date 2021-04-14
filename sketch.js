5c949323c7cacecef093d5c7babc2a05bf4d5403
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-04-14 17:46:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var fruit,rope;
+var fruit_con;
+
+var bg_img;
+var food;
+var rabbit;
+
+function preload()
+{
+  bg_img = loadImage('assets/background.png');
+  food = loadImage('assets/melon.png');
+  rabbit = loadImage('assets/Rabbit-01.png');
+}
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  imageMode(CENTER);
+  
+}
+
+function draw() 
+{
+  background(51);
+
+  image(bg_img,width/2,height/2,490,690);
+
+  image(food,fruit.position.x,fruit.position.y,70,70);
+  rope.show();
+  Engine.update(engine);
+  ground.show();
+
+ 
+   
+}
